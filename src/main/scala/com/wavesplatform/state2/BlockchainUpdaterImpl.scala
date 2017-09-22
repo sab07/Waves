477ce8ef31950fa7b070464eaa7289ec5083dba8
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2017-09-22 13:19:27 +0300

    NODE-185 Metrics: the block processing (+- transactions) and a size of UTX pool (updated)

--- src/main/scala/com/wavesplatform/state2/BlockchainUpdaterImpl.scala
+++ src/main/scala/com/wavesplatform/state2/BlockchainUpdaterImpl.scala
@@ -6,6 +6,7 @@ import cats._
 import cats.implicits._
 import com.wavesplatform.features.FeatureProvider
 import com.wavesplatform.history.HistoryWriterImpl
+import com.wavesplatform.metrics.TxsInBlockchainStats
 import com.wavesplatform.settings.FunctionalitySettings
 import com.wavesplatform.state2.BlockchainUpdaterImpl._
 import com.wavesplatform.state2.NgState._
@@ -97,6 +98,7 @@ class BlockchainUpdaterImpl private(persisted: StateWriter with StateReader,
             composite(currentPersistedBlocksState, () => referencedLiquidDiff.copy(heightDiff = 1)),
             Some(referencedForgedBlock), block))
             .map { hardenedDiff =>
+              TxsInBlockchainStats.record(ng.transactions.size)
               topMemoryDiff.transform(Monoid.combine(_, referencedLiquidDiff))
               (hardenedDiff, discarded.flatMap(_.transactionData))
             }
@@ -149,7 +151,9 @@ class BlockchainUpdaterImpl private(persisted: StateWriter with StateReader,
             }
           }
           logHeights(s"Rollback to h=$height completed:")
-          Right(discardedTransactions.result())
+          val r = discardedTransactions.result()
+          TxsInBlockchainStats.record(r.size)
+          Right(r)
       }
     }
   }
